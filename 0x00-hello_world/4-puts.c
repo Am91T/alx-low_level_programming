218299cf5a1dba6dca75429422230ebcf3c3b233
amine tiouk <tiouk.amine@gmail.com>
2023-07-13 16:09:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - this is quote
+*@return 0 on successful execution.
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
