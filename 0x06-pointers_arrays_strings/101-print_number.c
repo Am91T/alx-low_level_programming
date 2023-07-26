782ad5d6ca5825e959147fbeb5f6cd5f627d6a1f
amine tiouk <tiouk.amine@gmail.com>
2023-07-26 12:20:17 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,33 +7,19 @@
  */
 void print_number(int n)
 {
-	int digit, is_negative = 0;
-	int divisor = 1;
-	int temp = n;
+	unsigned int n1;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
+	n1 = n;
 
 	if (n < 0)
 	{
-		is_negative = 1;
-		n = -n;
-	}
-
-	while (temp /= 10)
-		divisor *= 10;
-
-	if (is_negative)
 		_putchar('-');
-
-	while (divisor)
-	{
-		digit = n / divisor;
-		_putchar('0' + digit);
-		n -= digit * divisor;
-		divisor /= 10;
+		n1 = -n;
 	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
