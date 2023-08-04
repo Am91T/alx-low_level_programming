3374bbd94dd39a7dec445edb57146f0497b8b9bd
amine tiouk <tiouk.amine@gmail.com>
2023-08-04 15:54:21 -0700

    libmy.a

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,18 @@
 #include "main.h"
 
+/**
+ * main - Entry point for the program
+ *
+ * Description:
+ * This function prints a quote
+ * by Kevin Mitnick using the custom "_puts" function.
+ *
+ * Return: 0 on successful execution
+ */
+
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to
+			be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
 }
